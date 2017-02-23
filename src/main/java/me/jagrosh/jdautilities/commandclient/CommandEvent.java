419fc65eac0a78df0b53d5de7aba4c7710e767a8
TheMonitorLizard <KaidanGustave@yahoo.com>
2017-02-22 19:16:21 -0600

    Update CommandEvent.java

--- src/main/java/me/jagrosh/jdautilities/commandclient/CommandEvent.java
+++ src/main/java/me/jagrosh/jdautilities/commandclient/CommandEvent.java
@@ -18,7 +18,6 @@ package me.jagrosh.jdautilities.commandclient;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import net.dv8tion.jda.client.entities.Group;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.MessageBuilder;
