15bd2715140165c1fdba76d9305b9c31a1ffa374
TheMonitorLizard <KaidanGustave@yahoo.com>
2017-02-23 16:29:13 -0600

    Final Child Commit.

--- src/main/java/me/jagrosh/jdautilities/commandclient/Command.java
+++ src/main/java/me/jagrosh/jdautilities/commandclient/Command.java
@@ -51,15 +51,12 @@ public abstract class Command {
     	// child check
         if(!event.getArgs().equalsIgnoreCase(event.getRawArgs()))
         {
-        	for(Command child : children)
+        	for(Command child : children) for(String cname : event.getRawArgs().replace(event.getArgs(), "").trim().split("\\s+"))
         	{
-        		for(int i = 0; i < event.getRawArgs().replaceFirst(event.getArgs(), "").trim().split("\\s+").length; i++)
+        		if(child.getName().equalsIgnoreCase(cname))
         		{
-        			if(child.getName().equalsIgnoreCase(event.getRawArgs().replaceFirst(event.getArgs(), "").trim().split("\\s+")[i]))
-        			{
-        				child.run(event);
-        				return;
-        			}
+        			child.run(event);
+        			return;
         		}
         	}
         }
@@ -192,6 +189,11 @@ public abstract class Command {
         return name;
     }
     
+    public String[] getAliases()
+    {
+    	return aliases;
+    }
+    
     public String getHelp()
     {
         return help;
@@ -211,7 +213,7 @@ public abstract class Command {
     {
     	return children;
     }
-
+   
     public Permission[] getUserPermissions()
     {
         return userPermissions;
