e6ced8564ff718b029fa206702c294fb2fbd94a8
TheMonitorLizard <kaidangustave@yahoo.com>
2017-11-27 12:46:32 -0600

    Fixed a oversight in Full User Reference Pattern for `FinderUtil`

--- src/main/java/com/jagrosh/jdautilities/utils/FinderUtil.java
+++ src/main/java/com/jagrosh/jdautilities/utils/FinderUtil.java
@@ -56,7 +56,7 @@ import java.util.stream.Collectors;
 public class FinderUtil
 {
     public final static Pattern DISCORD_ID = Pattern.compile("\\d{17,20}"); // ID
-    public final static Pattern FULL_USER_REF = Pattern.compile("(.{2,32})\\s*#(\\d{4})"); // $1 -> username, $2 -> discriminator
+    public final static Pattern FULL_USER_REF = Pattern.compile("(\\S.{0,30}\\S)\\s*#(\\d{4})"); // $1 -> username, $2 -> discriminator
     public final static Pattern USER_MENTION = Pattern.compile("<@!?(\\d{17,20})>"); // $1 -> ID
     public final static Pattern CHANNEL_MENTION = Pattern.compile("<#(\\d{17,20})>"); // $1 -> ID
     public final static Pattern ROLE_MENTION = Pattern.compile("<@&(\\d{17,20})>"); // $1 -> ID
