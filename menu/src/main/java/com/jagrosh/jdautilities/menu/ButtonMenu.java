6dff811674f62b0973772e36a088219ac6e8d8d7
TheMonitorLizard <kaidangustave@yahoo.com>
2018-01-15 23:51:05 -0600

    Added Kaidan Gustave to copyright

--- menu/src/main/java/com/jagrosh/jdautilities/menu/ButtonMenu.java
+++ menu/src/main/java/com/jagrosh/jdautilities/menu/ButtonMenu.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 John Grosh (jagrosh).
+ * Copyright 2016-2018 John Grosh (jagrosh) & Kaidan Gustave (TheMonitorLizard)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
