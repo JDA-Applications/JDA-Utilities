2d35642e0137960c83baaf018b32e88bd8b6ffa7
Andre_601 <11576465+Andre601@users.noreply.github.com>
2020-06-22 04:04:38 +0200

    Improve handling of Activity.

--- command/src/main/java/com/jagrosh/jdautilities/command/impl/CommandClientImpl.java
+++ command/src/main/java/com/jagrosh/jdautilities/command/impl/CommandClientImpl.java
@@ -473,6 +473,10 @@ public class CommandClientImpl implements CommandClient, EventListener
             return;
         }
         textPrefix = prefix.equals(DEFAULT_PREFIX) ? "@"+event.getJDA().getSelfUser().getName()+" " : prefix;
+        
+        if(status != null)
+            event.getJDA.getPresence().setOnlineStatus(status);
+            
         event.getJDA().getPresence().setPresence(status==null ? OnlineStatus.ONLINE : status, 
                 activity ==null ? null : "default".equals(activity.getName()) ? Activity.playing("Type "+textPrefix+helpWord) : activity);
 
